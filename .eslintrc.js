7b3d70d2a07368682e4ec6f9e5336beb9c79bc37
Fatemeh Majd <fmmajd452@gmail.com>
2018-04-04 12:05:06 +0430

    added more rules. removed json file

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,16 +8,29 @@ module.exports = {
 		sourceType: "module"
 	},
 	rules: {
-		indent: ["error", "tab"],
 		"linebreak-style": ["error", "unix"],
-		quotes: ["error", "double"],
-		semi: ["error", "always"],
 		"no-console": 0,
+
+		// syntax
 		"no-var": "error",
 		"prefer-arrow-callback": "error",
 		"prefer-template": "error",
 		"no-eval": "error",
 		"prefer-const": 1,
-		"no-inline-comments": 0
+		quotes: ["error", "double"],
+		semi: ["error", "always"],
+		"no-use-before-define": "error",
+		"func-style": ["error", "expression"],
+
+		// indent
+		indent: ["error", "tab"],
+
+		// spacing
+		"space-in-parens": [2, "always"],
+		"template-curly-spacing": [2, "always"],
+		"array-bracket-spacing": [2, "always"],
+		"object-curly-spacing": [2, "always"],
+		"computed-property-spacing": [2, "always"],
+		"no-multiple-empty-lines": [2, { max: 1, maxEOF: 0, maxBOF: 0 }]
 	}
 };
